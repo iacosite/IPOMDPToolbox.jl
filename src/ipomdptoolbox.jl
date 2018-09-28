dd1321f3c52e94c66941127b1a253c3f1363c251
Iacopo Olivo <iacopo.olivo@gmail.com>
2018-09-28 17:42:25 -0500

    Moved find functions here

--- src/ipomdptoolbox.jl
+++ src/ipomdptoolbox.jl
@@ -57,3 +57,21 @@ function generateGeneralPOMDP(ipomdp::IPOMDP, agent::Agent, model::pomdpModel)
     return null
 end
 
+# Find element X in the array V
+function find(V::Vector{Agent}, X::Agent)
+    for (i,e) in enumerate(X)
+        if(e == X)
+            return i
+        end
+    end
+    return -1
+end
+
+function find(V::Vector{Model}, X::Model)
+    for (i,e) in enumerate(X)
+        if(e == X)
+            return i
+        end
+    end
+    return -1
+end
